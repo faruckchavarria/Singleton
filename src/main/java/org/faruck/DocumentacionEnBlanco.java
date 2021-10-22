881b0ab5c1450766e1599881b20e97808085c15e
faruckchavarria <evafaruck@gmail.com>
2021-10-21 23:41:28 -0600

    Se muestra informacion de un array list

--- src/main/java/org/faruck/DocumentacionEnBlanco.java
+++ src/main/java/org/faruck/DocumentacionEnBlanco.java
@@ -1,2 +1,21 @@
-package org.faruck;public class DocumentacionEnBlanco {
+package org.faruck;
+
+
+import java.util.*;
+public class DocumentacionEnBlanco
+{
+    private static DocumentacionEnBlanco _instance = null;
+
+    private <Documento> DocumentacionEnBlanco()
+    {
+        new ArrayList();
+    }
+
+    public static DocumentacionEnBlanco Instance()
+    {
+        if (_instance == null)
+            _instance = new DocumentacionEnBlanco();
+        return _instance;
+    }
+
 }
